156e8277800565414b5d9eceddea1abd981383eb
Vettorazzo <marcos.vettorazzo@gmail.com>
2025-05-22 23:05:57 -0300

    env

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    ZABBIX_URL = os.environ.get('ZABBIX_URL', 'http://IP/api_jsonrpc.php')
+    ZABBIX_URL = os.environ.get('ZABBIX_URL', '')
     ZABBIX_TOKEN = os.environ.get('ZABBIX_TOKEN', '')
     REFRESH_INTERVAL = os.environ.get('REFRESH_INTERVAL', 60)  # in seconds
     DEBUG = os.environ.get('FLASK_DEBUG', False)
